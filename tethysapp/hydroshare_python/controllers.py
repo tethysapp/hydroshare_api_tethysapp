d7eeafe941d6d2ee92dcd2df45e5e2bc4564cc40
Abu <abu@tmplab-32-120-121.et.byu.edu>
2020-05-21 15:11:18 -0600

    Add file option updated to be more user friendly

--- tethysapp/hydroshare_python/controllers.py
+++ tethysapp/hydroshare_python/controllers.py
@@ -7,6 +7,11 @@ from tethys_sdk.gizmos import DataTableView
 from django.shortcuts import redirect, reverse
 from django.contrib import messages
 from hs_restclient import HydroShare, HydroShareAuthBasic
+import os
+import tempfile
+import zipfile
+
+
 auth = HydroShareAuthBasic(username='abhishekamal18@gmail.com', password='hydro1234')
 hs = HydroShare(auth=auth)
 
@@ -147,7 +152,7 @@ def get_file(request):
             abstract = date_built
             keywords = owner.split(', ')
             rtype = 'GenericResource'
-            fpath = '/Users/abu/Desktop/resources/tl_2017_06059_roads.shp'
+            fpath = '/Users/abu/Desktop/resources/tl_2017_06059_roads.shp' #fpath = 'tethysapp/geocode/workspaces/app_workspace/output.txt'
             metadata = '[{"coverage":{"type":"period", "value":{"start":"01/01/2000", "end":"12/12/2010"}}}, {"creator":{"name":"'+author+'"}}, {"creator":{"name":"'+coauthor+'"}}]'
             extra_metadata = '{"key-1": "value-1", "key-2": "value-2"}'
             resource_id = hs.createResource(rtype, title, resource_file=fpath, keywords=keywords, abstract=abstract, metadata=metadata, extra_metadata=extra_metadata)
@@ -225,12 +230,16 @@ def add_file(request):
     """
     # Default Values
     title = ''
+    # filename = ''
+    resourcein = ''
     # owner = 'Reclamation'
     # river = ''
     # date_built = ''
 
     # Errors
     title_error = ''
+    # filename_error = ''
+    resourcein_error = ''
     # owner_error = ''
     # river_error = ''
     # date_error = ''
@@ -239,29 +248,61 @@ def add_file(request):
     if request.POST and 'add-button' in request.POST:
         # Get values
         has_errors = False
+        # filename = request.POST.get('filename', None)
+        resourcein = request.POST.get('resourcein', None)
         title = request.POST.get('title', None)
-        # owner = request.POST.get('owner', None)
-        # river = request.POST.get('river', None)
-        # date_built = request.POST.get('date-built', None)
+        print(dict(request.FILES))
+        uploaded_file = request.FILES['addfile']
 
-        # Validate
-        if not title:
-            has_errors = True
-            title_error = 'Title is required.'
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_zip_path = os.path.join(temp_dir, title+'.shp')
+            print(temp_zip_path)
 
-        if not has_errors:
-            # Do stuff here
-            title = title
-            fpath = '/Users/abu/Desktop/resources/nyu_2451_34514.shp.zip'
-            resource_id = hs.addResourceFile('358a0196c5004d429a532158cff74d0e', fpath) #"remove_original_after_zip": True
-        
-        messages.error(request, "Please fix errors.")
+            # Use with statements to ensure opened files are closed when done
+            with open(temp_zip_path, 'wb') as temp_zip:
+                for chunk in uploaded_file.chunks():
+                    temp_zip.write(chunk)
+
+            # Validate
+            if not title:
+                has_errors = True
+                title_error = 'Title is required.'
+            
+            if not resourcein:
+                has_errors = True
+                resourcein_error = 'Resource is required.'
+
+            # if not filename:
+            #     has_errors = True
+            #     filename_error = 'Filename is required.'
+
+            if not has_errors:
+                # Do stuff here
+                # title = title
+                # filename = filename
+                fpath = temp_zip_path #'/Users/abu/Desktop/resources/nyu_2451_34514.shp.zip'
+                resource_id = hs.addResourceFile(resourcein, fpath) #"remove_original_after_zip": True
+                return redirect(reverse('hydroshare_python:home'))
+            #Utah Municipal resource id
+            messages.error(request, "Please fix errors.")
 
     # Define form gizmos
     title_input = TextInput(
         display_text='Title',
         name='title'
     )
+
+    resourcein_input = TextInput(
+        display_text='Resource ID',
+        name='resourcein',
+        placeholder='Enter id here eg: 08c6e88adaa647cd9bb28e5d619178e0 '
+    )
+
+    # filename_input = TextInput(
+    #     display_text='File name',
+    #     name='title',
+    #     placeholder='Enter the name of file here '
+    # )
 
     # owner_input = TextInput(
     #     display_text='Keywords',
@@ -299,6 +340,8 @@ def add_file(request):
 
     context = {
         'title_input': title_input,
+        'resourcein_input': resourcein_input,
+        # 'filename_input': filename_input,
         # 'owner_input': owner_input,
         # 'river_input': river_input,
         # 'date_built_input': date_built,
@@ -308,3 +351,145 @@ def add_file(request):
 
     return render(request, 'hydroshare_python/add_file.html', context)
 
+# @login_required()
+# def delete_file(request):
+#     """
+#     Controller for the Add Dam page.
+#     """
+#     # Default Values
+#     title = ''
+#     # filename = ''
+#     resourcein = ''
+#     # owner = 'Reclamation'
+#     # river = ''
+#     # date_built = ''
+
+#     # Errors
+#     title_error = ''
+#     # filename_error = ''
+#     resourcein_error = ''
+#     # owner_error = ''
+#     # river_error = ''
+#     # date_error = ''
+
+#     # Handle form submission
+#     if request.POST and 'add-button' in request.POST:
+#         # Get values
+#         has_errors = False
+#         # filename = request.POST.get('filename', None)
+#         resourcein = request.POST.get('resourcein', None)
+#         title = request.POST.get('title', None)
+#         print(dict(request.FILES))
+#         uploaded_file = request.FILES['addfile']
+
+#         with tempfile.TemporaryDirectory() as temp_dir:
+#             temp_zip_path = os.path.join(temp_dir, title+'.shp')
+#             print(temp_zip_path)
+
+#             # Use with statements to ensure opened files are closed when done
+#             with open(temp_zip_path, 'wb') as temp_zip:
+#                 for chunk in uploaded_file.chunks():
+#                     temp_zip.write(chunk)
+
+#             # try:
+#             #     # Extract the archive to the temporary directory
+#             #     with zipfile.ZipFile(temp_zip_path) as temp_zip:
+#             #         temp_zip.extractall(temp_dir)
+
+#             # except zipfile.BadZipFile:
+#             #     # Return error message
+#             #     return 'You must provide a zip archive containing a shapefile.'
+
+#             # owner = request.POST.get('owner', None)
+#             # river = request.POST.get('river', None)
+#             # date_built = request.POST.get('date-built', None)
+#             # resource = request.POST.get('resource', None)
+#             # filename = request.POST.get('filename', None)
+
+#             # Validate
+#             if not title:
+#                 has_errors = True
+#                 title_error = 'Title is required.'
+            
+#             if not resourcein:
+#                 has_errors = True
+#                 resourcein_error = 'Resource is required.'
+
+#             # if not filename:
+#             #     has_errors = True
+#             #     filename_error = 'Filename is required.'
+
+#             if not has_errors:
+#                 # Do stuff here
+#                 # title = title
+#                 # filename = filename
+#                 fpath = temp_zip_path #'/Users/abu/Desktop/resources/nyu_2451_34514.shp.zip'
+#                 resource_id = hs.addResourceFile(resourcein, fpath) #"remove_original_after_zip": True
+#                 return redirect(reverse('hydroshare_python:home'))
+#             #Utah Municipal resource id
+#             messages.error(request, "Please fix errors.")
+
+#     # Define form gizmos
+#     title_input = TextInput(
+#         display_text='Title',
+#         name='title'
+#     )
+
+#     resourcein_input = TextInput(
+#         display_text='Resource ID',
+#         name='resourcein',
+#         placeholder='Enter id here eg: 08c6e88adaa647cd9bb28e5d619178e0 '
+#     )
+
+#     # filename_input = TextInput(
+#     #     display_text='File name',
+#     #     name='title',
+#     #     placeholder='Enter the name of file here '
+#     # )
+
+#     # owner_input = TextInput(
+#     #     display_text='Keywords',
+#     #     name='owner',
+#     #     placeholder='eg: shapefiles, datasets, etc..'
+#     # ) 
+
+#     # river_input = TextInput(
+#     #     display_text='Name of Creator',
+#     #     name='river',
+#     #     placeholder='e.g: John Smith'
+#     # )
+
+#     # date_built = TextInput(
+#     #     display_text='Keywords',
+#     #     name='date-built',
+#     #     placeholder='e.g: Shapefiles, datasets, etc..'
+    
+#     # )
+
+#     add_button = Button(
+#         display_text='Add',
+#         name='add-button',
+#         icon='glyphicon glyphicon-plus',
+#         style='success',
+#         attributes={'form': 'add-dam-form'},
+#         submit=True
+#     )
+
+#     cancel_button = Button(
+#         display_text='Cancel',
+#         name='cancel-button',
+#         href=reverse('hydroshare_python:home')
+#     )
+
+#     context = {
+#         'title_input': title_input,
+#         'resourcein_input': resourcein_input,
+#         # 'filename_input': filename_input,
+#         # 'owner_input': owner_input,
+#         # 'river_input': river_input,
+#         # 'date_built_input': date_built,
+#         'add_button': add_button,
+#         'cancel_button': cancel_button,
+#     }
+
+#     return render(request, 'hydroshare_python/delete_file.html', context)
